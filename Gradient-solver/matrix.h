5093da821740449127704136a0d27b35678614b0
aidian3k <adrian.nowosielski1@gmail.com>
2022-01-04 22:29:34 +0100

    fixed bugs

--- Gradient-solver/matrix.h
+++ Gradient-solver/matrix.h
@@ -1,12 +1,13 @@
 #ifndef _MAT_IO_H
 #define _MAT_IO_H
+#include <stdio.h>
 
 typedef struct _Matrix {
 		int r,c;
 		double **data;
 } Matrix;
 
-Matrix * readFromFile(char * fname);
+Matrix * readFromFile(FILE *in);
 
 void printToScreen(Matrix *mat);
 
@@ -27,5 +28,11 @@ double dot_product2(Matrix *mat1, Matrix *mat2);
 void multiplybynumMatrix(Matrix *ans,Matrix *mat1,double num);
 
 void copyMatrix(Matrix *ans,Matrix *mat);
+
+void putentryMatrix(Matrix* mat, int i, int j, double num);
+
+void addtoentryMatrix(Matrix *mat, int i, int j, double num);
+
+double getentryMatrix(Matrix* mat, int i, int j );
 
 #endif
